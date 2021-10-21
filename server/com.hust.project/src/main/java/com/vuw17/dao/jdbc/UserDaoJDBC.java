a2d587468bc56f081712c7dd63fd9c2fd66dc72c
thekhoa2206 <thekhoa2206@gmail.com>
2021-10-21 22:51:11 +0700

    them jdbc

--- server/com.hust.project/src/main/java/com/vuw17/dao/jdbc/UserDaoJDBC.java
+++ server/com.hust.project/src/main/java/com/vuw17/dao/jdbc/UserDaoJDBC.java
@@ -2,11 +2,14 @@ package com.vuw17.dao.jdbc;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
+import javax.transaction.Transactional;
 
-@Service("UserDaoJDBC")
+@Repository("UserDaoJDBC")
+@Transactional(rollbackOn = Exception.class)
 public class UserDaoJDBC extends JdbcDaoSupport {
     private final JdbcTemplate jdbcTemplateObject;
 
